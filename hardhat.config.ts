6e73ec76644223ccd84c559f5330e3744d4dd444
Adam Skrodzki <adamskrodzki@gmail.com>
2021-11-05 23:40:58 +0100

    config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -57,7 +57,7 @@ const config: HardhatUserConfig = {
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: true,
     currency: "USD",
   },
   etherscan: {
