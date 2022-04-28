6284e5469dc1445dc6e87ccac0a4fd2dd2c0a042
Roman Krasiuk <rokrassyuk@gmail.com>
2022-04-28 19:57:08 +0300

    fix duplicate code

--- scripts/common/one-inch.ts
+++ scripts/common/one-inch.ts
@@ -25,28 +25,21 @@ export async function getSwap(
     slippage: BigNumber,
     debug = false,
 ) {
-    if (debug)
-        console.log('One inch params', {
-            fromTokenAddress: collateralAddress,
-            toTokenAddress: daiAddress,
-            amount: amount.toFixed(0),
-            fromAddress: sender,
-            slippage: slippage.times(100).toString(),
-            disableEstimate: true,
-            allowPartialFill: false,
-            protocols: 'UNISWAP_V3,PMM4,UNISWAP_V2,SUSHI,CURVE,PSM',
-        })
+    const params = {
+        fromTokenAddress: collateralAddress,
+        toTokenAddress: daiAddress,
+        amount: amount.toFixed(0),
+        fromAddress: sender,
+        slippage: slippage.toString(),
+        disableEstimate: true,
+        allowPartialFill: false,
+        protocols: 'UNISWAP_V3,PMM4,UNISWAP_V2,SUSHI,CURVE,PSM',
+    }
+
+    if (debug) console.log('One inch params', params)
+
     const { data } = await axios.get<OneInchSwapResponse>(`${API_ENDPOINT}/swap`, {
-        params: {
-            fromTokenAddress: collateralAddress,
-            toTokenAddress: daiAddress,
-            amount: amount.toFixed(0),
-            fromAddress: sender,
-            slippage: slippage.toString(),
-            disableEstimate: true,
-            allowPartialFill: false,
-            protocols: 'UNISWAP_V3,PMM4,UNISWAP_V2,SUSHI,CURVE,PSM',
-        },
+        params,
     })
 
     if (debug) console.log('One inch payload', data)
@@ -56,7 +49,7 @@ export async function getSwap(
     return {
         collateralAmount,
         daiAmount,
-        tokenPrice: daiAmount.div(collateralAmount), // .times(new BigNumber(100).minus(slippage).div(100)),
+        tokenPrice: daiAmount.div(collateralAmount),
         tx: data.tx,
         fromToken: data.fromToken,
         toToken: data.toToken,
