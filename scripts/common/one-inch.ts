43194401ea36be67eb86483f564a9507ced14256
Adam Skrodzki <adam@oazoapps.com>
2022-05-06 10:44:30 +0200

    revert

--- scripts/common/one-inch.ts
+++ scripts/common/one-inch.ts
@@ -14,8 +14,7 @@ export async function getQuote(daiAddress: string, collateralAddress: string, am
     })
     const collateralAmount = new BigNumber(data.fromTokenAmount).shiftedBy(-data.fromToken.decimals)
     const daiAmount = new BigNumber(data.toTokenAmount).shiftedBy(-data.toToken.decimals)
-    const price = daiAmount.div(collateralAmount)
-    return price
+    return daiAmount.div(collateralAmount);
 }
 
 export async function getSwap(
