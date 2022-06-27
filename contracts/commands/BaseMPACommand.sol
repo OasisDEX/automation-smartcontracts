52d81582534b96a9f7e5fb019e52b82a2470faf3
Roman Krasiuk <rokrassyuk@gmail.com>
2022-06-27 15:40:16 +0300

    CHORE: MPA Base Cleanup (#80)
    * mpa base cleanup
    
    * rm unused func

--- contracts/commands/BaseMPACommand.sol
+++ contracts/commands/BaseMPACommand.sol
@@ -40,7 +40,7 @@ abstract contract BaseMPACommand {
         serviceRegistry = _serviceRegistry;
     }
 
-    function getBasicVaultAndMarketInfo(uint256 cdpId)
+    function getVaultAndMarketInfo(uint256 cdpId)
         public
         view
         returns (
@@ -59,36 +59,26 @@ abstract contract BaseMPACommand {
         nextPrice = mcdView.getNextPrice(ilk);
     }
 
-    function getBasicTriggerDataInfo(bytes memory triggerData)
-        public
-        pure
-        virtual
-        returns (uint256 cdpId, uint16 triggerType);
+    function validateTriggerType(uint16 triggerType, uint16 expectedTriggerType) public pure {
+        require(triggerType == expectedTriggerType, "base-mpa-command/type-not-supported");
+    }
 
-    function validateTriggerType(
-        bytes memory triggerData,
-        bytes memory executionData,
-        uint16 expectedTriggerType,
-        bytes4 expectedSelector
-    ) public pure {
-        (, uint16 triggerType) = getBasicTriggerDataInfo(triggerData);
-        require(triggerType == expectedTriggerType, "mpa-command-base/type-not-supported");
-
+    function validateSelector(bytes4 expectedSelector, bytes memory executionData) public pure {
         bytes4 selector = abi.decode(executionData, (bytes4));
-        require(selector == expectedSelector, "mpa-command-base/invalid-selector");
+        require(selector == expectedSelector, "base-mpa-command/invalid-selector");
     }
 
     function executeMPAMethod(bytes memory executionData) internal {
-        (bool status, bytes memory errorMsg) = serviceRegistry
+        (bool status, bytes memory reason) = serviceRegistry
             .getRegisteredService(MPA_KEY)
             .delegatecall(executionData);
-        require(status, string(errorMsg));
+        require(status, string(reason));
     }
 
-    function reregisterTrigger(
-        bytes memory triggerData,
+    function recreateTrigger(
         uint256 cdpId,
-        uint16 triggerType
+        uint16 triggerType,
+        bytes memory triggerData
     ) internal {
         (bool status, ) = msg.sender.delegatecall(
             abi.encodeWithSelector(
@@ -99,6 +89,6 @@ abstract contract BaseMPACommand {
                 triggerData
             )
         );
-        require(status, "mpa-command-base/trigger-recreation-failed");
+        require(status, "base-mpa-command/trigger-recreation-failed");
     }
 }
