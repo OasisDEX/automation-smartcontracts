5d41bd8d293652dc0297d1c570cb53d4231353ee
Adam Skrodzki <adam@oazoapps.com>
2022-09-05 11:26:07 +0200

    fix test

--- test/basic-sell.ts
+++ test/basic-sell.ts
@@ -111,7 +111,7 @@ describe('BasicSellCommand', () => {
             await expect(createTrigger(triggerData, false)).to.be.reverted
         })
 
-        it.skip('should fail if trigger type is not encoded correctly', async () => {
+        it('should fail if trigger type is not encoded correctly', async () => {
             //NOT relevant anymore as theres is no triggerType to compare to, command is chosen based on triggerType in triggerData
             const triggerData = utils.defaultAbiCoder.encode(
                 ['uint256', 'uint16', 'uint256', 'uint256', 'uint256', 'bool'],
