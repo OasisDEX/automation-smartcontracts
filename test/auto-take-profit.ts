191304c3bd083a21ea4117d33a19c67157620229
halaprix <panicz.klos@gmail.com>
2022-12-09 10:42:02 +0100

    fix

--- test/auto-take-profit.ts
+++ test/auto-take-profit.ts
@@ -304,7 +304,7 @@ describe('AutoTakeProfitCommmand', async () => {
                     )
                 })
 
-                it('should refund transaction costs if sufficient balance available on AutomationExecutor', async () => {
+                it('should refund transaction costs if sufficient balance available on AutomationExecutor [ @skip-on-coverage ]', async () => {
                     await hre.ethers.provider.getSigner(2).sendTransaction({
                         to: AutomationExecutorInstance.address,
                         value: EthersBN.from(10).pow(18),
@@ -538,7 +538,7 @@ describe('AutoTakeProfitCommmand', async () => {
                     expect(collateral.toNumber()).to.be.equal(0)
                 })
 
-                it('should refund transaction costs if sufficient balance available on AutomationExecutor', async () => {
+                it('should refund transaction costs if sufficient balance available on AutomationExecutor [ @skip-on-coverage ]', async () => {
                     await hre.ethers.provider.getSigner(2).sendTransaction({
                         to: AutomationExecutorInstance.address,
                         value: EthersBN.from(10).pow(18),
