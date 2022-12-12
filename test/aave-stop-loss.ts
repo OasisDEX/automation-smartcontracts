48c96c14498c5a70776db155a874b66c549c3f8e
halaprix <panicz.klos@gmail.com>
2022-12-12 10:17:51 +0100

    refactor test

--- test/aave-stop-loss.ts
+++ test/aave-stop-loss.ts
@@ -1,5 +1,5 @@
 import hre, { ethers } from 'hardhat'
-import { BigNumber as EthersBN, Contract, utils } from 'ethers'
+import { BigNumber as EthersBN, Contract, Signer, utils } from 'ethers'
 import {
     AutomationBot,
     DsProxyLike,
@@ -17,6 +17,7 @@ import { AccountGuard } from '../typechain/AccountGuard'
 import BigNumber from 'bignumber.js'
 import { setBalance } from '@nomicfoundation/hardhat-network-helpers'
 import { TriggerGroupType } from '@oasisdex/automation'
+import { expect } from 'chai'
 
 describe.only('AaveStoplLossCommand', async () => {
     /* this can be anabled only after whitelisting us on OSM */
@@ -27,12 +28,15 @@ describe.only('AaveStoplLossCommand', async () => {
     let MPAInstance: MPALike
     let usersProxy: DsProxyLike
     let proxyOwnerAddress: string
+    let proxyAddress: string
     let receiverAddress: string
     let executorAddress: string
     let snapshotId: string
     let aaveStopLoss: AaveStoplLossCommand
     let aavePool: ILendingPool
-
+    let aave_pa: AaveProxyActions
+    let receiver: Signer
+    let account: IAccountImplementation
     before(async () => {
         await hre.network.provider.request({
             method: 'hardhat_reset',
@@ -48,7 +52,7 @@ describe.only('AaveStoplLossCommand', async () => {
         const system = await deploySystem({ utils: hardhatUtils, addCommands: true })
         // executor is the deployer
         const executor = hre.ethers.provider.getSigner(0)
-        const receiver = hre.ethers.provider.getSigner(1)
+        receiver = hre.ethers.provider.getSigner(1)
         executorAddress = await executor.getAddress()
         receiverAddress = await receiver.getAddress()
         setBalance(receiverAddress, EthersBN.from(1000).mul(EthersBN.from(10).pow(18)))
@@ -57,9 +61,10 @@ describe.only('AaveStoplLossCommand', async () => {
         automationBotInstance = system.automationBot
         automationExecutorInstance = system.automationExecutor
         aaveStopLoss = system.aaveStoplLossCommand!
+        aave_pa = system.aaveProxyActions as AaveProxyActions
 
         const factory = system.dpmFactory as AccountFactory
-        const aave_pa = system.aaveProxyActions as AaveProxyActions
+
         const guard = system.accountGuard as AccountGuard
 
         const factoryReceipt = await (
@@ -67,16 +72,13 @@ describe.only('AaveStoplLossCommand', async () => {
         ).wait()
 
         const [AccountCreatedEvent] = getEvents(factoryReceipt, factory.interface.getEvent('AccountCreated'))
-        const proxyAddress = AccountCreatedEvent.args.proxy.toString()
+        proxyAddress = AccountCreatedEvent.args.proxy.toString()
         console.log('aaveStopLoss', aaveStopLoss.address)
         console.log('receiverAddress', receiverAddress)
         console.log('proxyAddress', proxyAddress)
         console.log('bot', automationBotInstance.address)
         console.log('executor', automationExecutorInstance.address)
-        const account = (await hre.ethers.getContractAt(
-            'IAccountImplementation',
-            proxyAddress,
-        )) as IAccountImplementation
+        account = (await hre.ethers.getContractAt('IAccountImplementation', proxyAddress)) as IAccountImplementation
         // whitelist aave proxy actions
         await guard.connect(executor).setWhitelist(aave_pa.address, true)
         await guard.connect(executor).setWhitelist(automationBotInstance.address, true)
@@ -98,61 +100,12 @@ describe.only('AaveStoplLossCommand', async () => {
             EthersBN.from(1000).mul(EthersBN.from(10).pow(6)),
         ])
 
-        const drawDebtReceipt = await (
+        await (
             await account.connect(receiver).execute(aave_pa.address, encodedDrawDebtData, {
                 gasLimit: 3000000,
             })
         ).wait()
-        let userData = await aavePool.getUserAccountData(proxyAddress)
-        const ltv = userData.totalDebtETH.mul(1000).div(userData.totalCollateralETH)
 
-        // 3. close vault using FL
-        const aToken = await ethers.getContractAt('ERC20', '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e')
-        const aTokenBalance = await aToken.balanceOf(proxyAddress)
-        console.log('aToken balance', aTokenBalance)
-
-        const amountInWei = aTokenBalance
-        const fee = EthersBN.from(20)
-        const feeBase = EthersBN.from(10000)
-        const data = await getSwap(
-            hardhatUtils.addresses.WETH,
-            hardhatUtils.addresses.USDC,
-            receiverAddress,
-            new BigNumber(aTokenBalance.toString()),
-            new BigNumber('10'),
-        )
-        //console.log(data)
-
-        await hardhatUtils.setTokenBalance(
-            aave_pa.address,
-            hardhatUtils.addresses.WETH,
-            hre.ethers.utils.parseEther('10'),
-        )
-        const exchangeData = {
-            fromAsset: hardhatUtils.addresses.WETH_AAVE,
-            toAsset: hardhatUtils.addresses.USDC,
-            amount: amountInWei.add(amountInWei.mul(fee).div(feeBase)),
-            receiveAtLeast: 0,
-            fee: fee,
-            withData: data.tx.data,
-            collectFeeInFromToken: false,
-        }
-        const aaveData = {
-            debtTokenAddress: hardhatUtils.addresses.USDC,
-            collateralTokenAddress: hardhatUtils.addresses.WETH_AAVE,
-            fundsReceiver: receiverAddress,
-        }
-
-        const serviceRegistry = {
-            aaveProxyActions: aave_pa.address,
-            lender: receiverAddress,
-            exchange: hardhatUtils.addresses.SWAP,
-        }
-        const encodedClosePositionData = aave_pa.interface.encodeFunctionData('closePosition', [
-            exchangeData,
-            aaveData,
-            serviceRegistry,
-        ])
         // dont close for now use automation
         /*         const closePositionReceipt = await (
             await account.connect(receiver).execute(aave_pa.address, encodedClosePositionData, {
@@ -160,42 +113,10 @@ describe.only('AaveStoplLossCommand', async () => {
             })
         ).wait() */
 
-        userData = await aavePool.getUserAccountData(proxyAddress)
+        // userData = await aavePool.getUserAccountData(proxyAddress)
 
-        const trigerDataTypes = ['address', 'uint16', 'address', 'address', 'uint256', 'uint32']
-        const trigerDecodedData = [
-            proxyAddress,
-            10,
-            hardhatUtils.addresses.WETH,
-            hardhatUtils.addresses.USDC,
-            ltv.sub(1),
-            300,
-        ]
-        const triggerData = utils.defaultAbiCoder.encode(trigerDataTypes, trigerDecodedData)
-
-        const dataToSupply = automationBotInstance.interface.encodeFunctionData('addTriggers', [
-            TriggerGroupType.SingleTrigger,
-            [false],
-            [0],
-            [triggerData],
-            [10],
-        ])
-        // add trigger
-        const tx = await account.connect(receiver).execute(automationBotInstance.address, dataToSupply)
         //executionData = generateTpOrSlExecutionData(MPAInstance, true, cdpData, exchangeData, serviceRegistry)
         // execute trigger
-        const tx2 = await automationExecutorInstance.execute(
-            encodedClosePositionData,
-            0,
-            triggerData,
-            aaveStopLoss.address,
-            '1',
-            '0',
-            '0',
-            178000,
-            hardhatUtils.addresses.USDC,
-            { gasLimit: 3000000 },
-        )
     })
 
     describe('isTriggerDataValid', () => {
@@ -208,8 +129,81 @@ describe.only('AaveStoplLossCommand', async () => {
         })
 
         describe('closeToCollateral operation', async () => {
+            let encodedClosePositionData: string
+            let triggerData: string
             before(async () => {
-                console.log('befre')
+                const userData = await aavePool.getUserAccountData(proxyAddress)
+                const ltv = userData.totalDebtETH.mul(1000).div(userData.totalCollateralETH)
+
+                // 3. close vault using FL
+                const aToken = await ethers.getContractAt('ERC20', hardhatUtils.addresses.AAVE_AWETH_TOKEN)
+                const aTokenBalance = await aToken.balanceOf(proxyAddress)
+                console.log('aToken balance', aTokenBalance.toString())
+
+                const amountInWei = aTokenBalance
+                const fee = EthersBN.from(20)
+                const feeBase = EthersBN.from(10000)
+                const data = await getSwap(
+                    hardhatUtils.addresses.WETH,
+                    hardhatUtils.addresses.USDC,
+                    receiverAddress,
+                    new BigNumber(aTokenBalance.toString()),
+                    new BigNumber('10'),
+                )
+
+                await hardhatUtils.setTokenBalance(
+                    aave_pa.address,
+                    hardhatUtils.addresses.WETH,
+                    hre.ethers.utils.parseEther('10'),
+                )
+                const exchangeData = {
+                    fromAsset: hardhatUtils.addresses.WETH_AAVE,
+                    toAsset: hardhatUtils.addresses.USDC,
+                    amount: amountInWei.add(amountInWei.mul(fee).div(feeBase)),
+                    receiveAtLeast: 0,
+                    fee: fee,
+                    withData: data.tx.data,
+                    collectFeeInFromToken: false,
+                }
+                const aaveData = {
+                    debtTokenAddress: hardhatUtils.addresses.USDC,
+                    collateralTokenAddress: hardhatUtils.addresses.WETH_AAVE,
+                    fundsReceiver: receiverAddress,
+                }
+
+                const serviceRegistry = {
+                    aaveProxyActions: aave_pa.address,
+                    lender: receiverAddress,
+                    exchange: hardhatUtils.addresses.SWAP,
+                }
+                encodedClosePositionData = aave_pa.interface.encodeFunctionData('closePosition', [
+                    exchangeData,
+                    aaveData,
+                    serviceRegistry,
+                ])
+
+                const trigerDataTypes = ['address', 'uint16', 'address', 'address', 'uint256', 'uint32']
+                const trigerDecodedData = [
+                    proxyAddress,
+                    10,
+                    hardhatUtils.addresses.WETH,
+                    hardhatUtils.addresses.USDC,
+                    ltv.sub(1),
+                    300,
+                ]
+                triggerData = utils.defaultAbiCoder.encode(trigerDataTypes, trigerDecodedData)
+
+                const dataToSupply = automationBotInstance.interface.encodeFunctionData('addTriggers', [
+                    TriggerGroupType.SingleTrigger,
+                    [false],
+                    [0],
+                    [triggerData],
+                    [10],
+                ])
+                // add trigger
+                await account.connect(receiver).execute(automationBotInstance.address, dataToSupply)
+
+                //console.log(data)
             })
 
             describe('when Trigger is below current col ratio', async () => {
@@ -221,8 +215,22 @@ describe.only('AaveStoplLossCommand', async () => {
                     console.log('after each')
                 })
 
-                it('should revert trigger execution', async () => {
-                    console.log('should')
+                it('should execute trigger', async () => {
+                    await automationExecutorInstance.execute(
+                        encodedClosePositionData,
+                        0,
+                        triggerData,
+                        aaveStopLoss.address,
+                        '1',
+                        '0',
+                        '0',
+                        178000,
+                        hardhatUtils.addresses.USDC,
+                        { gasLimit: 3000000 },
+                    )
+                    const userData = await aavePool.getUserAccountData(proxyAddress)
+                    expect(userData.totalCollateralETH).to.be.eq(0)
+                    expect(userData.totalDebtETH).to.be.eq(0)
                 })
             })
         })
