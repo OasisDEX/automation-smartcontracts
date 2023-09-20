36a64a391c62f863227d873e9960c593d2ad5b2f
halaprix <panicz.klos@gmail.com>
2023-09-20 16:34:28 +0200

    chore: add docs

--- scripts/common/safe.ts
+++ scripts/common/safe.ts
@@ -7,6 +7,11 @@ import SafeServiceClient from '@safe-global/safe-service-client'
 import { DeployedSystem } from './deploy-system'
 import chalk from 'chalk'
 
+/**
+ * Adds a registry entry using multisig.
+ * @param safeTransactionData - The transaction data to be executed on the safe.
+ * @param network - The network on which the transaction will be executed.
+ */
 export const addRegistryEntryMultisig = async (
     safeTransactionData: SafeTransactionDataPartial | MetaTransactionData[],
     network: Network,
@@ -44,6 +49,15 @@ export enum SafeTxType {
     setWhitelist,
 }
 
+/**
+ * Returns an array of partial transaction data based on the provided parameters.
+ * @param system - The deployed system object.
+ * @param type - The type of safe transaction.
+ * @param address - The address to be added to the service registry or whitelist.
+ * @param hash - The hash of the named service to be added to the service registry.
+ * @param nameOrTriggerId - The name of service or trigger ID of the named service to be added to the service registry.
+ * @returns An array of partial transaction data.
+ */
 export function getSafePartialTransactionData(
     system: DeployedSystem,
     type: SafeTxType,
